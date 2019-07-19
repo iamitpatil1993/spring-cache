fdace1b10605960dee2c8404b9add365b5a71f0d
Amit Patil <iamitpatil1993@gmail.com>
2019-07-19 07:37:47 +0530

    Ehcache:P-2: Xml configuration

--- src/main/java/com/example/spring/cahce/configuration/cache/EhcacheCacheProviderConfiguration.java
+++ src/main/java/com/example/spring/cahce/configuration/cache/EhcacheCacheProviderConfiguration.java
@@ -4,9 +4,13 @@ import org.ehcache.CacheManager;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.xml.XmlConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.net.URL;
 
 /**
  * @author amit
@@ -27,6 +31,23 @@ public class EhcacheCacheProviderConfiguration {
         // create cache programmatically.
         cacheManager.createCache("basicTestCache", cacheConfiguration);
 
+        return cacheManager;
+    }
+
+    /**
+     * Xml based configuration for CacheManager and Cache used in application.
+     * This cache manager can be used in exactly same way as we used 'prorammatically' configured cache manager.
+     *
+     * @return
+     */
+    @Bean
+    @Qualifier(value = "xml")
+    @Primary
+    public CacheManager ehcachNativeXmlCacheManager() {
+        final URL myUrl = getClass().getResource("/ehcache.xml");
+        final XmlConfiguration xmlConfig = new XmlConfiguration(myUrl);
+        final CacheManager cacheManager = CacheManagerBuilder.newCacheManager(xmlConfig);
+        cacheManager.init();
         return cacheManager;
     }
 }
